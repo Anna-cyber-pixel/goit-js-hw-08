259ca19f114888c3ff4c048be25cfdbcba890c82
Anna Kravets <superpower390@gmail.com>
2022-11-20 20:33:01 +0200

    2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,23 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItem({ preview, original, description }) {
+  return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  navText: ['⬅️', '➡️'],
+  closeText: '❎',
+});
